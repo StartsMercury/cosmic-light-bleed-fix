ddcfcd472bf34eb7853c050029e31636c11ffb52
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-09-16 21:23:32 +0800

    Update Cosmic Loom to 1.0.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
         classpath(
             group = "org.codeberg.CRModders",
             name = "cosmic-loom",
-            version = "9a9771504f91bedb731eb7c71029b87e25a18f2c",
+            version = "1.0.2",
         )
     }
 }
