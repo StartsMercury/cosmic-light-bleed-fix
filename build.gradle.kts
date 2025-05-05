3f35e0cff3516bd0edd44b07e41e520912312dfb
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-05-05 14:13:24 +0800

    Update to Cosmic Reach 0.4.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ object Constants {
 
     const val DISPLAY_NAME = "Cosmic Light Bleed Fix"
 
-    const val VERSION_COSMIC_REACH = "0.4.7"
+    const val VERSION_COSMIC_REACH = "0.4.8"
     const val VERSION_JAVA = "17"
 }
 
